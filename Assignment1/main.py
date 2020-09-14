851c7427154d035f37769e854511ea1323b0d9c6
JakeP627 <60146366+JakeP627@users.noreply.github.com>
2020-09-13 20:07:44 -0500

    Fixed the kmer in kmerPatern code to make it work correctly

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -13,28 +13,19 @@ def FrequentWordsProblem(inp,k):
             kmerPatern.append(kmer)
             counts.append(1)
         elif kmer in kmerPatern:
+            # we need to go to the first spot where that value occurs
             kmerPatern.append(kmer)
-            #we need to go to the first spot where that value occurs
+            counts.append(0)
+            counts[kmerPatern.index(kmer)] += 1
         else:
             kmerPatern.append(kmer)
             counts.append(1)
 
-        # elif kmer in kmerPatern:
-        #     counts.append(0)
-        #     counts[kmerPatern.index(kmer)] += 1
-
-
-    # for x, y in zip(kmerPatern, counts):
-    #      print(x,y)
-    #print(kmerPatern)
-    # print(counts)
-    #print(len(counts))
-
     m = max(counts)
     output = []
-    for val in counts:
+    for i, val in enumerate(counts):
         if val == m:
-            output.append(kmerPatern[counts.index(val)])
+            output.append(kmerPatern[i])
 
     print(output)
 
@@ -46,6 +37,5 @@ FrequentWordsProblem(input1, 3)
 FrequentWordsProblem(input2, 4)
 FrequentWordsProblem(input3, 5)
 FrequentWordsProblem(input4, 5)
-#print(len(input0))
 
 
